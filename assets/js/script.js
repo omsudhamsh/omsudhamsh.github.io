3fff670ef00136565e0e7a198d6bd47fffd576d0
omsudhamshstu <ommsstudentnetwork@gmail.com>
2025-01-02 20:25:15 +0530

    Changes#1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const navLinks = document.querySelectorAll('[data-nav-link]');
-//   const pages = document.querySelectorAll('[data-page]');
+document.addEventListener('DOMContentLoaded', () => {
+  const navLinks = document.querySelectorAll('[data-nav-link]');
+  const pages = document.querySelectorAll('[data-page]');
 
-//   navLinks.forEach(link => {
-//     link.addEventListener('click', () => {
-//       const targetPage = link.textContent.trim().toLowerCase();
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      const targetPage = link.textContent.trim().toLowerCase();
 
-//       navLinks.forEach(nav => nav.classList.remove('active'));
-//       link.classList.add('active');
+      navLinks.forEach(nav => nav.classList.remove('active'));
+      link.classList.add('active');
 
-//       pages.forEach(page => {
-//         if (page.dataset.page === targetPage) {
-//           page.classList.add('active');
-//         } else {
-//           page.classList.remove('active');
-//         }
-//       });
-//     });
-//   });
-// });
+      pages.forEach(page => {
+        if (page.dataset.page === targetPage) {
+          page.classList.add('active');
+        } else {
+          page.classList.remove('active');
+        }
+      });
+    });
+  });
+});
 
 'use strict';
 
